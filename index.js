f84e9cfb96962bc48edcf759a5aebd735f5234c4
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-11-08 01:43:31 +0600

    mongodb uri client setup done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const cors = require ('cors')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -7,11 +8,24 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+//mongodb 
+
+const uri = "mongodb+srv://<username>:<password>@clusterm01.jgnnfze.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+
+
 //simple api call
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('service review app server is running well..')
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is running on port ${port}`)
 })
